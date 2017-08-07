71086feabd0c5da85de59b9185253e7c5993c9a4
Shahzaib Ather <shahzaibather7@github.ca>
2017-08-07 11:20:08 -0700

    Initial Commit

---
+++ ScrollViewImageGallaries/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollViewImageGallaries
+//
+//  Created by Mohammad Shahzaib Ather on 2017-08-07.
+//  Copyright © 2017 Mohammad Shahzaib Ather. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
